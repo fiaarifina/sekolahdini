888663324fa8472a137633fc926f4c754fb592b3
fiaarifina <diniarifina03@gmail.com>
2023-01-12 12:11:50 +0700

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,9 @@
+<!-- footer -->
+    <div class="footer">
+        <div class="container text-center">
+            Copyright &copy; 2022 -  <?= $d->nama ?>.
+        </div>
+    </div>
+
+    </body>
+</html>
